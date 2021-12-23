aa4a0cf539f3057aeb52e5df77352946325539ea
devarsh-addweb <devarsh.addweb@gmail.com>
2021-12-23 18:59:01 +0530

    Weather Using Node

---
+++ weatherApp/config.js
@@ -0,0 +1,11 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        // SECRET_KEY: "49851e0a7859639bf80e7d237fd280e6"
+        SECRET_KEY: "2d02963b1541f1a461d0bdb7a1fb311f"
+    }
+}
+
+module.exports = constants;
+
+// 2d02963b1541f1a461d0bdb7a1fb311f
